ff3a9f33b4b7ca3615b92914eb90c8f5e1b619a2
casperzapototsky <casperzapototsky@gmail.com>
2022-11-18 16:16:58 +0200

    testThatYoungUsersCanNotPlay method is added, failingtests branch is added

--- src/test/java/absoft/wombat/AgeCheckerTest.java
+++ src/test/java/absoft/wombat/AgeCheckerTest.java
@@ -8,6 +8,13 @@ public class AgeCheckerTest {
     @Test
     public void testAgedUserCanPlay(){
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.canPlayGame(20),"Your age doesn't allow you to proceed to next step!");
+        Assert.assertTrue(ageChecker.canPlayGame(22),"Your age doesn't allow you to proceed to next step!");
     }
+
+    @Test
+    public void testThatTooYongUsersCanNotPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(7),"Children under 18 not allowed to play game! If you are 18 years old, Welcome!");
+    }
+
 }
